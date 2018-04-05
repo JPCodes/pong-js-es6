950555b3d5290a08ca2329e1fd4a593546c5afc4
Jim Padilla <jp_code2013@yahoo.com>
2018-04-05 13:30:25 -0700

    Pong ball movement

--- pong.js
+++ pong.js
@@ -22,14 +22,37 @@ class Ball extends Rect { // Ball inherits from the Rect class
 const canvas = document.getElementById('pong');
 const context = canvas.getContext('2d');
 const ball = new Ball;
+
 ball.pos.x = 100;
 ball.pos.y = 50;
 
-// Game Board options
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+ball.vel.x = 100;
+ball.vel.y = 100;
 
-// Ball options
-context.fillStyle = '#fff';
-context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y); // Ball Positioning & Sizing
+let lastTime; // Last Time page was loaded
 
+function callback(millis) { // Milliseconds coming from animation frame
+  if (lastTime) {
+    update((millis - lastTime) / 1000); // Calc how much time has elapsed & convert to whole seconds 
+  }
+  lastTime = millis;
+  requestAnimationFrame(callback); // Request Animation Frame calls callback once, so we use recursion here
+}
+
+function update(dt) { // Delta Time; Function to Redraw Pong
+  ball.pos.x += ball.vel.x * dt;
+  ball.pos.y += ball.vel.y * dt;
+
+  // Game Board options
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  
+  // Ball options
+  context.fillStyle = '#fff';
+  context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y); // Ball Positioning & Sizing
+}
+
+callback();
+
+// Run Flow:
+// Create Board & Ball, Set options (pos, vel, size), callback called manually once, callback runs recursively
