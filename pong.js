2778bb48bdcb0d6ecd0a9f66a2624aaa528aa718
Jim Padilla <jp_code2013@yahoo.com>
2018-04-08 13:26:22 -0700

    Reset ball when scoring occurs

--- pong.js
+++ pong.js
@@ -124,9 +124,8 @@ class Pong {
     // Detect if Ball touches bounds of Canvas
     if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
       const playerId = this.ball.vel.x < 0 | 0; // Results in integer 0 or 1
-      console.log(playerId);
-      
-      // this.ball.vel.x = -this.ball.vel.x; // Velocity inversion
+      this.players[playerId].score++
+      this.reset();
     }
 
     if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
